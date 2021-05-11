f2e7c5eb7745583f3ba90c4f3717b2c4a19f36c4
joao <joaopedro37@gmail.com>
2021-05-11 15:16:20 -0300

    atividade finalizada ;)

--- exercicio_2/Program.cs
+++ exercicio_2/Program.cs
@@ -6,45 +6,62 @@ namespace exercicio_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual combustivel você deseja abastecer?(A-álcool; G-gasolina");
-            string tipo = Console.ReadLine().ToLower();
-            Console.WriteLine("Quanto vc deseja abastecer em litros ?");
-            double litros = double.Parse(Console.ReadLine());
+
 
             double alcool = 4.90;
             double gasolina = 5.30;
-
-           switch (tipo)
-           {
-               case "a":
-                    if (litros <= 20)
-                    {
-                        double litroA = (alcool*litros);
-                        double valor = litroA*0.97;
-                        Console.WriteLine("O valor com desconsto ficou de " + valor + "reais");
-                    }else{
-                        double litroA = (alcool*litros);
-                        double valor = litroA*0.95;
-                        Console.WriteLine("O valor com desconsto ficou de " + valor + "reais");
-                    }
-                   break;
-                case "g":
+            bool consicao = false;
+            int litros;
+            
+            do
+            {
+                Console.WriteLine("Qual combustivel você deseja abastecer?(A-álcool; G-gasolina");
+                string tipo = Console.ReadLine().ToLower();
+                switch (tipo)
+                {
+                    case "a":
+                        Console.WriteLine("Quanto vc deseja abastecer em litros ?");
+                        litros = int.Parse(Console.ReadLine());
+                        if (litros <= 20)
+                        {
+                            double litroA = (alcool * litros);
+                            double valor = litroA * 0.97;
+                            Console.WriteLine("O valor com desconsto ficou de " + valor.ToString("N2") + "reais");
+                        }
+                        else
+                        {
+                            double litroA = (alcool * litros);
+                            double valor = litroA * 0.95;
+                            Console.WriteLine("O valor com desconsto ficou de " + valor.ToString("N2") + "reais");
+                        }
+                        consicao = true;
+                        break;
+                    case "g":
+                        Console.WriteLine("Quanto vc deseja abastecer em litros ?");
+                        litros = int.Parse(Console.ReadLine());
                         if (litros > 20)
                         {
-                            double litroG = (gasolina*litros);
-                            double valorG = litroG*0.96;
-                            Console.WriteLine("O valor com desconsto ficou de " + valorG + "reais");
-                        }else{
-                            double litroG = (gasolina*litros);
-                            double valorG = litroG*0.94;
-                            Console.WriteLine("O valor com desconsto ficou de " + valorG + "reais");
+                            double litroG = (gasolina * litros);
+                            double valorG = litroG * 0.96;
+                            Console.WriteLine("O valor com desconsto ficou de " + valorG.ToString("N2") + "reais");
                         }
-                    break;
-               default:
-                   break;
-           }
-           
-            
+                        else
+                        {
+                            double litroG = (gasolina * litros);
+                            double valorG = litroG * 0.94;
+                            Console.WriteLine("O valor com desconsto ficou de " + valorG.ToString("N2") + "reais");
+                        }
+                        consicao = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida , digite uma opção valida!");
+                        break;
+                }
+            } while (consicao == false);
+
+
+
+
         }
     }
 }
